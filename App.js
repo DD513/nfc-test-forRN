73d64fa2e091f85b082aa671221c4a9938b818c2
Tilda <tilda.lu@trunk-studio.com>
2021-11-01 18:19:08 +0800

    feat: 🎉 nfc test
    nfc test app

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Button, Text, View } from "react-native";
+import NfcManager, { NfcEvents } from "react-native-nfc-manager";
 
 export default function App() {
+  // Pre-step, call this before any NFC operations
+  async function initNfc() {
+    await NfcManager.start();
+    console.log("start");
+  }
+
+  function readNdef() {
+    const cleanUp = () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+      NfcManager.setEventListener(NfcEvents.SessionClosed, null);
+    };
+
+    return new Promise((resolve) => {
+      let tagFound = null;
+
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+        tagFound = tag;
+        resolve(tagFound);
+        NfcManager.setAlertMessageIOS("NDEF tag found");
+        NfcManager.unregisterTagEvent().catch(() => 0);
+      });
+
+      NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+        cleanUp();
+        if (!tagFound) {
+          resolve();
+        }
+      });
+
+      NfcManager.registerTagEvent();
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test NFC@!</Text>
+      <Button
+        onPress={() => readNdef()}
+        mode="contained"
+        style={{ paddingTop: 32 }}
+        title="Open NFC"
+        color="#000"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +55,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
