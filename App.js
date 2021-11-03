53cf2e7b385ebd953fd84d43a1143e9d323ab778
Lu Tilda <tilda.lu@trunk-studio.com>
2021-11-03 18:24:29 +0800

    feat: 🎨 android reader
    add reader function for android test, but doesn't test iOS yet

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Button, Text, View } from "react-native";
-import NfcManager, { NfcEvents } from "react-native-nfc-manager";
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
 
 export default function App() {
   // Pre-step, call this before any NFC operations
@@ -10,37 +10,60 @@ export default function App() {
     console.log("start");
   }
 
-  function readNdef() {
-    const cleanUp = () => {
-      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
-      NfcManager.setEventListener(NfcEvents.SessionClosed, null);
-    };
+  async function readMifare() {
+    try {
+      // 0. Request Mifare technology
+      let reqMifare = await NfcManager.requestTechnology(
+        NfcTech.MifareUltralight
+      );
+      if (reqMifare !== "MifareUltralight") {
+        throw new Error(
+          "[NFC Read] [ERR] Mifare technology could not be requested"
+        );
+      }
 
-    return new Promise((resolve) => {
-      let tagFound = null;
+      // 1. Get NFC Tag information
+      const nfcTag = await NfcManager.getTag();
+      console.log("[NFC Read] [INFO] Tag: ", nfcTag);
 
-      NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
-        tagFound = tag;
-        resolve(tagFound);
-        NfcManager.setAlertMessageIOS("NDEF tag found");
-        NfcManager.unregisterTagEvent().catch(() => 0);
-      });
+      // 2. Read pages
+      const readLength = 60;
+      let mifarePages = [];
+      const mifarePagesRead = await Promise.all(
+        [...Array(readLength).keys()].map(async (_, i) => {
+          const pageOffset = i * 4; // 4 Pages are read at once, so offset should be in steps with length 4
+          let pages =
+            await NfcManager.mifareUltralightHandlerAndroid.mifareUltralightReadPages(
+              pageOffset
+            );
+          mifarePages.push(pages);
+          console.log(`[NFC Read] [INFO] Mifare Page: ${pageOffset}`, pages);
+          //await wait(500); // If Mifare Chip is to slow
+        })
+      );
 
-      NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
-        cleanUp();
-        if (!tagFound) {
-          resolve();
-        }
-      });
+      // 3. Success
+      console.log("[NFC Read] [INFO] Success reading Mifare");
 
-      NfcManager.registerTagEvent();
-    });
+      // 4. Cleanup
+      _cleanup();
+    } catch (ex) {
+      console.warn("[NFC Read] [ERR] Failed Reading Mifare: ", ex);
+      _cleanup();
+    }
   }
+
+  function _cleanup() {
+    NfcManager.cancelTechnologyRequest().catch(() => 0);
+  }
+
+  initNfc();
+
   return (
     <View style={styles.container}>
       <Text>Test NFC@!</Text>
       <Button
-        onPress={() => readNdef()}
+        onPress={() => readMifare()}
         mode="contained"
         style={{ paddingTop: 32 }}
         title="Open NFC"
