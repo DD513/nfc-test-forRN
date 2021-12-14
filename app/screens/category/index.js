8183846c61e4db9638f92347e05148f3e5fb1406
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-12-14 09:54:03 +0800

    feat: :sparkles: remake category page
    only transplant basic components of view

--- app/screens/category/index.js
+++ app/screens/category/index.js
@@ -1,50 +1,197 @@
-import React from 'react';
-import { StyleSheet,View,Dimensions } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, ScrollView, Image, TextInput } from "react-native";
+import styles from "./styles.js";
+import { AppLoading } from "expo-app-loading";
+import { useFonts, Roboto_500Medium } from "@expo-google-fonts/roboto";
 
-// Dimensions 用於獲取裝置寬、高、解析度
-const { width, height } = Dimensions.get("window");
+import {
+  Button,
+  Flex,
+  Icon,
+  // IconFill, 
+  // IconOutline,
+  InputItem,
+  List,
+  SwipeAction,
+  Provider,
+  WingBlank,
+  Modal,
+} from "@ant-design/react-native";
 
-export default function category({route,navigation}) {
-    let path;
-    let url = 'https://fintess-coach.herokuapp.com';
-    if (route) {
-        console.log(route);
-        if (route.params) {
-            if (route.params.id) {
-                path = `/${route.name}?id=${route.params.id}`;
-                url = url + path;
-            } else if (route.params.url) {
-                url = route.params.url;
-                console.log(url);
-            }
-        } else if (route.url !== null) {
-            url = route.url;
-        }
-    }
-    console.log(123,url);
-    if (url === 'https://fintess-coach.herokuapp.com') {
-        if (route.canGoBack) {
-            navigation.goBack();
-        } else {
-            navigation.navigate('NFCReader');
-        }
-    }
-    return (
-      <View style={styles.container}>
-        <WebView
-        style={{width: width, height: height}}
-        source={{ uri: url }} 
-        />
+export default Category = ({ navigation }) => {
+  const right = [
+    {
+      text: <Icon name="delete" style={styles.deleteButton} />,
+      onPress: () => console.log("delete"),
+    },
+  ];
+
+  /* bug happened! */
+  // // To avoid rendering text before the font is loaded, install the expo-app-loading package to use the <AppLoading /> component: https://stackoverflow.com/questions/33971221/google-fonts-in-react-native
+  // let [fontsLoaded] = useFonts({
+  //   Roboto_500Medium,
+  // });
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // } else {
+  return (
+    <ScrollView>
+      <View style={styles.divBlock}>
+        <Flex justify="between" align="center" style={styles.titleFrame}>
+          <Text style={styles.categoryInfo}>Category</Text>
+          <Flex style={styles.categoryIcons} justify="between">
+            <Icon
+              style={styles.categoryIcon}
+              size={36}
+              color="#1890FF"
+              name="bell"
+            />
+            <Icon
+              style={styles.categoryIcon}
+              size={36}
+              color="#1890FF"
+              name="play-circle"
+            />
+          </Flex>
+        </Flex>
+
+        <View style={styles.categoryList}>
+          <Flex
+            justify="between"
+            align="center"
+            direction="row"
+            style={styles.categoryDynamic}
+          >
+            <Flex styles={styles.categoryCol}>
+              <Text style={styles.fieldSubtitle}>
+                <Text style={styles.fieldChinese}>組數</Text>
+                <Text style={styles.fieldEnglish}>Sets</Text>
+              </Text>
+            </Flex>
+            <Flex styles={styles.categoryCol}>
+              <Text style={styles.fieldSubtitle}>
+                <Text style={styles.fieldChinese}>公斤</Text>
+                <Text style={styles.fieldEnglish}>KG</Text>
+              </Text>
+            </Flex>
+            <Flex styles={styles.categoryCol}>
+              <Text style={styles.fieldSubtitle}>
+                <Text style={styles.fieldChinese}>次數</Text>
+                <Text style={styles.fieldEnglish}>Reps</Text>
+              </Text>
+            </Flex>
+          </Flex>
+          <ScrollView style={styles.randerBlock}>
+            <List>
+              {/* {_.map(renderData, (item, index) => ( */}
+              <SwipeAction
+                autoClose
+                style={styles.swipeAction}
+                right={right}
+                onOpen={() => console.log("open")}
+                onClose={() => console.log("close")}
+              >
+                <Flex
+                  justify="between"
+                  align="center"
+                  direction="row"
+                  style={styles.categoryInputRow}
+                >
+                  <View>
+                    <Button
+                      style={styles.categoryInputButton}
+                      type="ghost"
+                      shape="circle"
+                    >
+                      {/* {++index} */}
+                    </Button>
+                  </View>
+                  {/* {this.changeRenderKg(index)}
+                  {this.changeRenderReps(index)} */}
+                  <View>
+                    <TextInput
+                      style={styles.categoryInputButtonItem}
+                      // value={this.state.number}
+                      keyboardType="numeric"
+                    />
+                  </View>
+                  <View>
+                    <TextInput
+                      style={styles.categoryInputButtonItem}
+                      // value={this.state.number}
+                      keyboardType="numeric"
+                    />
+                  </View>
+                </Flex>
+              </SwipeAction>
+              {/* ))} */}
+            </List>
+          </ScrollView>
+          <View style={styles.timerBlock}>
+            <View style={styles.rowContent}>
+              <Flex.Item style={styles.timerStatus}>
+                <Image
+                  style={styles.timerStatusButton}
+                  /* rendering svg is different from web */
+                  // source={timerStatus === '運動時間' ? `${images.workout}` : `${images.break}`}
+                />
+              </Flex.Item>
+
+              <Flex justify="center" align="center" style={styles.timer}>
+                <Text style={styles.timerText}>
+                  <Text style={styles.minute}>00</Text>
+                  <Text style={styles.semicolon}>:</Text>
+                  <Text style={styles.second}>00</Text>
+                </Text>
+              </Flex>
+
+              <View style={styles.colContent}>
+                <Provider>
+                  <Button style={(styles.startButton, styles.trainingButton)}>
+                    開始/休息
+                  </Button>
+                </Provider>
+              </View>
+
+              <View style={(styles.rowContent, styles.equipmentInfo)}>
+                <Flex
+                  style={styles.equipmentInfoColContent}
+                  align="center"
+                  justify="start"
+                >
+                  <Icon style={styles.equipmentInfoIcon} name="aim" size={14} />
+                  <Text style={styles.equipmentInfoName}>地點</Text>
+                </Flex>
+                <Flex
+                  style={styles.equipmentInfoColContent}
+                  align="center"
+                  justify="start"
+                >
+                  <Icon
+                    style={styles.equipmentInfoIcon}
+                    name="barcode"
+                    size={14}
+                  />
+                  <Text style={styles.equipmentInfoName}>器材</Text>
+                </Flex>
+              </View>
+
+              <Flex.Item style={styles.colContent}>
+                <Button style={(styles.completeButton, styles.trainingButton)}>
+                  完成訓練
+                </Button>
+              </Flex.Item>
+              <Flex.Item style={styles.colContent}>
+                <Button style={(styles.cancelButton, styles.trainingButton)}>
+                  取消訓練
+                </Button>
+              </Flex.Item>
+            </View>
+          </View>
+        </View>
+        {/* <Modal></Modal> */}
       </View>
-    )
+    </ScrollView>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-});
+// };
