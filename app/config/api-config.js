23f8eb13f42decb6da0084f1c16aed6dd0534738
DD <50895419+DD513@users.noreply.github.com>
2022-01-06 15:09:03 +0800

    react-native-config util/request

---
+++ app/config/api-config.js
@@ -0,0 +1,14 @@
+/* App config for apis
+ */
+import Config from "react-native-config";
+const { BASE_URL } = Config;
+console.log("=== BASE_URL ===", BASE_URL);
+
+const ApiConfig = {
+  BASE_URL,
+  //USER
+
+  //CATEGORY
+};
+
+export default ApiConfig;
